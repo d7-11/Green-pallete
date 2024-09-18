32a166f93c0b8503658dbced4d10768bc3989105
d7-11 <danielanosova@gmail.com>
2024-09-18 14:15:56 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const swiper = new Swiper('.swiper', {
+
+    slidesPerView: 2,
+    spaceBetween: 22,
+
+    navigation: {
+      nextEl: '.reviews__item-next',
+      prevEl: '.reviews__item-prev',
+    },
+
+  });
