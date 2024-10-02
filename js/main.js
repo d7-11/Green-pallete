c60a7f90832b1bdb5e4600cae6f49d4d13a8d931
d7-11 <danielanosova@gmail.com>
2024-10-02 13:13:06 +0300

    Responsive design for small screens

--- js/main.js
+++ js/main.js
@@ -1,18 +1,39 @@
 const swiper = new Swiper('.swiper', {
 
-    slidesPerView: 2,
-    spaceBetween: 22,
+  slidesPerView: 2,
+  spaceBetween: 22,
 
-    navigation: {
-      nextEl: '.reviews__item-next',
-      prevEl: '.reviews__item-prev',
-    },
+  navigation: {
+    nextEl: '.reviews__item-next',
+    prevEl: '.reviews__item-prev',
+  },
 
-  });
+});
 
 const menuBtn = document.querySelector('.menu-btn');
 const menuList = document.querySelector('.menu__list');
 
-menuBtn.addEventListener('click', () =>{
+menuBtn.addEventListener('click', () => {
   menuList.classList.toggle('menu--open');
+});
+
+
+const accordeonTitle = document.querySelectorAll('.accordeon__title');
+
+accordeonTitle.forEach(item => {
+  item.addEventListener('click', () => {
+
+    const parent = item.parentNode;
+
+    if (parent.classList.contains('accordeon__item-active')) {
+      parent.classList.remove('accordeon__item-active')
+    } else {
+      accordeonTitle.forEach(element => {
+        element.parentNode.classList.remove('accordeon__item-active')
+      });
+
+      parent.classList.add('accordeon__item-active')
+    }
+
+  })
 });
